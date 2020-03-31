a7d059f06c4f94f1a3c17ed16011e765c93340e8
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2020-03-31 09:21:38 -0300

    Testando roteamento no nodes

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const http = require('http');
+const fs = require("fs");
 
-const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(function(req, res){
+const path = req.url;
+
+if(path=== "/json"){
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Primeiro Servidor em Nodes');
+  res.setHeader('Content-Type', "text/html");
+  res.end(JSON.stringify({phrase: "Servidor em JSON"}));
+} else if(path === "/html"){
+  res.statusCode = 200;
+  res.setHeader('Content-Type', "text/html");
+  const data = fs.readFileSync("/index.html","utf-8");
+  res.end(data);
+}else{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Primeiro Servidor em Nodes');
+}
+
+server.listen(port,() => {
+  console.log(`Server running at port:${port}/`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at port:${port}/`);
+
 });
